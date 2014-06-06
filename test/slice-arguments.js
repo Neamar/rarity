d5e1041e3a098a5ca0d0874f6e1daeb8cce3c273
Neamar <neamar@neamar.fr>
2014-06-06 16:48:27 +0200

    TDD again

--- test/slice-arguments.js
+++ test/slice-arguments.js
@@ -20,4 +20,23 @@ describe('Argument slicer', function() {
       done();
     }));
   });
+
+  for(var i = 0; i < 4; i += 1) {
+    it("rarity(" + i + ", cb)", function(done) {
+      shittyFunction(rarity(i, function() {
+        arguments.should.have.lengthOf(i);
+        done();
+      }));
+    });
+  }
+
+  it("should work with not enough argument", function(done) {
+    shittyFunction(rarity(200, function(err, result) {
+      should(err).eql(null);
+      result.should.eql(EXPECTED_RESULT);
+      arguments.should.have.lengthOf(4);
+      done();
+    }));
+  });
+
 });
